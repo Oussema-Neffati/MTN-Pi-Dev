16e63ff20fdb323381e7911fab077c965be8eb48
Neffati Oussema <oussema.neff@gmail.com>
2025-05-04 15:19:45 +0100

    commit

---
+++ src/main/java/tn/esprit/test/MainFX.java
@@ -0,0 +1,31 @@
+package tn.esprit.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+
+public class MainFX extends Application {
+    public void start(Stage stage) {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("/tn.esprit/LoginView.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) {launch();}
+
+}
+
+
