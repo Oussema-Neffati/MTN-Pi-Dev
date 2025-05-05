2ed9cc4553fdd365c7d39109bbf5292529d8ea60
Neffati Oussema <oussema.neff@gmail.com>
2025-05-05 18:01:29 +0100

    images

--- src/main/java/tn/esprit/test/MainFX.java
+++ src/main/java/tn/esprit/test/MainFX.java
@@ -12,7 +12,7 @@ import static javafx.application.Application.launch;
 public class MainFX extends Application {
     public void start(Stage stage) {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("/tn.esprit/LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("/FXML/LoginView.fxml"));
         Scene scene = null;
         try {
             scene = new Scene(fxmlLoader.load());
